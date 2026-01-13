b21406e6854664185ab18bf01b656cd1b83e2ab2
fvaldes0109 <fvaldes0109@gmail.com>
2026-01-13 22:10:07 +0100

    First commit

---
+++ marking-module/main.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def main():
+    print("Marking Module Started...")
+
+    array = np.array([1, 2, 3, 4, 5])
+    print(f"Numpy Array: {array}")
+    print(f"Mean value: {np.mean(array)}")
+
+if __name__ == "__main__":
+    main()
